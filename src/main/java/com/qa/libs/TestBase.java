babb5545d3122fb42cca66e3206d242b5ab4898e
humera_rk <humera.khatib@gmail.com>
2019-03-25 09:18:05 -0700

    added search and updated login

--- src/main/java/com/qa/libs/TestBase.java
+++ src/main/java/com/qa/libs/TestBase.java
@@ -1,4 +1,11 @@
 package com.qa.libs;
+//git init
+//git add remote origin <url>
+//git status
+//git add .
+//git status
+//git commit -m
+//git push origin master
 
 public class TestBase {
 	
--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,5 +4,10 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("Home Page");
 	}
+		
+		public void loginset() {
+			System.out.println("Login set values");
+	}
+	
 
 }
